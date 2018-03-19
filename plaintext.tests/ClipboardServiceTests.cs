9048fd247038bd3bfafed906d544b014fe841b0f
johnbt <johnbt@outlook.com>
2018-03-19 23:52:34 +0100

    get text from keyboard

--- plaintext.tests/ClipboardServiceTests.cs
+++ plaintext.tests/ClipboardServiceTests.cs
@@ -1,19 +1,29 @@
 ﻿using NUnit.Framework;
+using plaintext.services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Shouldly;
 
 namespace plaintext.tests
 {
 	[TestFixture]
 	public class ClipboardServiceTests
 	{
+		private ClipboardService clipboardService;
+
+		[SetUp]
+		public void SetUp()
+		{
+			clipboardService = new ClipboardService();
+		}
+
 		[Test]
 		public void TestClipboardServiceGetText()
 		{
-			
+			//var text = clipboardService.GetText();
 		}
 	}
 }
