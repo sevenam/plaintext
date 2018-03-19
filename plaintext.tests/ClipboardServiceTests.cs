803612e0ee72c88e5922b03bf57fddb3b038fccb
johnbt <johnbt@outlook.com>
2018-03-19 23:22:02 +0100

    add test project

---
+++ plaintext.tests/ClipboardServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace plaintext.tests
+{
+	[TestFixture]
+	public class ClipboardServiceTests
+	{
+		[Test]
+		public void TestClipboardServiceGetText()
+		{
+			
+		}
+	}
+}
