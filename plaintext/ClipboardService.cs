1d53d354745e1416c529bc5bf8d4f6e23dfc1c71
johnbt <johnbt@outlook.com>
2018-03-19 23:14:21 +0100

    add clipboardservice

---
+++ plaintext/ClipboardService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace plaintext
+{
+	public class ClipboardService
+	{
+		public ClipboardService()
+		{
+
+		}
+
+		public string GetText()
+		{
+			return Clipboard.GetText();
+		}
+
+	}
+}
