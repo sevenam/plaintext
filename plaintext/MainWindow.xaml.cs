8652803ba167b07a075d32c0ad8cecb6c03275ce
johnbt <johnbt@outlook.com>
2018-03-23 22:46:57 +0100

    add inputsimulator for simulating ctrl-v

--- plaintext/MainWindow.xaml.cs
+++ plaintext/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WindowsInput;
 
 namespace plaintext
 {
@@ -26,16 +27,19 @@ namespace plaintext
 		{
 			InitializeComponent();
 			Hide();
-			var hotkey = new HotkeyService(Key.Escape, KeyModifier.Shift, OnHotKeyHandler); //todo: would like this to use Win+V - but not working on mac at least
+			var hotkey = new HotkeyService(Key.Escape, KeyModifier.Win, OnHotKeyHandler); //todo: would like this to use Win+V - but not working on mac at least
 		}
 
 		private void OnHotKeyHandler(HotkeyService hotKey)
 		{
 			var clipboardService = new ClipboardService();
 			var text = clipboardService.GetText();
+            clipboardService.SetText(text);
 			Debug.WriteLine($"clipboard data:" + text);
+            var test = new InputSimulator();
+            test.Keyboard.ModifiedKeyStroke(WindowsInput.Native.VirtualKeyCode.SHIFT, WindowsInput.Native.VirtualKeyCode.INSERT);
 
-		}
+        }
 
 	}
 }
