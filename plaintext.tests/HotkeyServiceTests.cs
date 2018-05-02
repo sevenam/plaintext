80e40326cdc35e442211f6fd3e021add5a0bf487
johnbt <johnbt@outlook.com>
2018-05-02 21:14:43 +0200

    wip - add hotkeyservice tests

---
+++ plaintext.tests/HotkeyServiceTests.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using plaintext.services;
+using System.Windows.Input;
+using Shouldly;
+using WindowsInput;
+
+namespace plaintext.tests
+{
+    [TestFixture]
+    public class HotkeyServiceTests
+    {
+        private Key key;
+        private KeyModifier keyModifier;
+        private bool eventExecuted;
+
+        [SetUp]
+        public void SetUp()
+        {
+            key = Key.Escape;
+            keyModifier = KeyModifier.Win;
+        }
+
+        [Test]
+        public void TestHotKeyServiceInit()
+        {
+            var hotkeyService = new HotkeyService(key, keyModifier, HotkeyEvent, true);
+            hotkeyService.Key.ShouldBe(key);
+            hotkeyService.KeyModifiers.ShouldBe(keyModifier);
+            hotkeyService.SuccessfullyRegistered.ShouldBeTrue();
+            hotkeyService.Dispose();
+        }
+
+        [Test]
+        public void TestHotkeyServiceDispose()
+        {
+            var hotkeyService = new HotkeyService(key, keyModifier, HotkeyEvent, true);
+            hotkeyService.SuccessfullyRegistered.ShouldBeTrue();
+
+            hotkeyService.Dispose();
+            hotkeyService.SuccessfullyRegistered.ShouldBeFalse();
+        }
+
+        [Test]
+        public void TestHotkeyServiceExecuteKeyPress()
+        {
+            //todo: this test isn't working
+            var hotkeyService = new HotkeyService(key, keyModifier, HotkeyEvent, true);
+            hotkeyService.SuccessfullyRegistered.ShouldBeTrue();
+            var inputSimulator = new InputSimulator();
+
+            eventExecuted.ShouldBeFalse();
+            inputSimulator.Keyboard.ModifiedKeyStroke(WindowsInput.Native.VirtualKeyCode.LWIN, WindowsInput.Native.VirtualKeyCode.ESCAPE);
+            eventExecuted.ShouldBeTrue();
+
+            hotkeyService.Dispose();
+        }
+
+        private void HotkeyEvent(HotkeyService hotKey)
+        {
+            eventExecuted = true;
+        }
+
+    }
+}
